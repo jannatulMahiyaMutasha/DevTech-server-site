1f585be05c2ed9e14df3872f5aa78f482e5f577c
jannatulMahiyaMutasha <mutashamahiya678@gmail.com>
2025-05-21 22:07:54 +0600

    code change

--- index.js
+++ index.js
@@ -22,3 +22,20 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+
+function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(403).send("Unauthorized");
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).send("Invalid token");
+  }
+}
