e81bfc810709223784d76dc0882e7f079a4ddbfb
jannatulMahiyaMutasha <mutashamahiya678@gmail.com>
2025-05-21 22:06:34 +0600

    mongobd add

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ const JWT_SECRET = "123456";
 
 app.use(express.json());
 app.use(cors());
+
+const uri =
+  "mongodb+srv://freelance:SJ5HW66Mk5XOobot@cluster0.ahhvv5a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
